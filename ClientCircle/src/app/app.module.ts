a1aaf90959e53d9ad24cdb497e169753e595bd84
Reneetjuh21 <r.vangool@live.nl>
2019-06-06 12:50:58 +0200

    added required components

--- ClientCircle/src/app/app.module.ts
+++ ClientCircle/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { StreamComponent } from './components/stream/stream.component';
+import { ErrorComponent } from './components/error/error.component';
+import { HeadComponent } from './components/head/head.component';
+import { ChatComponent } from './components/chat/chat.component';
+import { PlayerComponent } from './components/player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    OverviewComponent,
+    StreamComponent,
+    ErrorComponent,
+    HeadComponent,
+    ChatComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule,

3895218683b6433dd585c7058cfd11bece987ebb
Reneetjuh21 <r.vangool@live.nl>
2019-06-11 13:08:51 +0200

    alerts added
    alerts added for too many streams selected and when a stream is already selected.

--- src/app/components/overview/overview.component.ts
+++ src/app/components/overview/overview.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { STREAMS } from '../../mock/streams.mock';
 import { Stream } from '../../models/stream';
+import { Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 declare var $: any;
 
@@ -15,20 +17,28 @@ export class OverviewComponent implements OnInit {
   streams:Stream[] = STREAMS;
   selectedStreams:Stream[] = [];
 
+  message: string;
+
   constructor() {
   }
 
   ngOnInit() {
     $('.selected-streams-overlay').hide();
+    $('.modal').hide();
   }
 
-  addToSelectedStreams(stream:Stream):boolean {
+  addToSelectedStreams(stream:Stream):void {
     if (this.selectedStreams.length < 4){
-      this.selectedStreams.push(stream);
-      this.showSelectedStreams();
-      return true;
+      if (!this.selectedStreams.find(x => x.id == stream.id)){
+        this.selectedStreams.push(stream);
+        this.showSelectedStreams();
+      }
+      else {
+        this.notifyUser('alreadySelected');
+      }
+    } else {
+      this.notifyUser('moreThanFour');
     }
-    return false;
   }
 
   showSelectedStreams():void {
@@ -38,7 +48,31 @@ export class OverviewComponent implements OnInit {
   removeFromSelectedStreams(stream:Stream):boolean {
     this.selectedStreams.splice(this.selectedStreams.indexOf(stream), 1);
     this.showSelectedStreams();
+    if (this.selectedStreams.length == 0){
+      $('.selected-streams-overlay').hide();
+    }
     return true;
+  }
+
+  changeMessage(message:string) {
+    this.message = message;
+    $('.modal').show();
+  }
+
+  closeModal() {
+    $('.modal').hide();
+  }
+
+  notifyUser(messageType:string):void {
+    switch(messageType){
+      case 'alreadySelected': {
+        this.changeMessage("You already selected this stream. Pick a different stream.");
+      }
+      case 'moreThanFour': {
+        this.changeMessage("You can only select a maximum of four streams. Deselect a stream before selecting another.");
+      }
+
+    }
   }
 
 }

f145f854f365b708f269e9833a763b3cfaaa27b7
MrSpaceblaze <32390661+MrSpaceblaze@users.noreply.github.com>
2019-06-18 20:27:30 +0200

    removed unecessary OnInit call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 
 @Component({
@@ -6,13 +6,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'ClientCircle';
 
   constructor(){
 
   }
 
-  ngOnInit() {
-  }
 }

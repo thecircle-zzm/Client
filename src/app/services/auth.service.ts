060204f83e9d8872d05449d7930c9a27b23e2968
René van Gool <r.vangool@live.nl>
2019-06-18 20:28:41 +0200

    RSA SHA256 sign function added
    with a privateKey.PEM file it should be able to sign with your username and key from now.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,8 @@ import { NgxIndexedDB } from 'ngx-indexed-db';
 import 'rxjs/add/operator/map';
 import { CertificateService } from './certificate.service';
 
+declare var KJUR: any;
+
 // import { UserService } from './user.service';
 
 // import { User } from '../models/user';
@@ -12,23 +14,30 @@ import { CertificateService } from './certificate.service';
 
 @Injectable()
 export class AuthenticationService {
-  // private users: User[];
   
   constructor(
     private http: HttpClient,
     private certificateService: CertificateService
-    // private userService: UserService
     ) { }
 
   login(username: string, key: string) {
     sessionStorage.setItem('user', JSON.stringify({username: username, key: key}))
 
-    let test = "Hello"
-    let signature = this.certificateService.sign(test);
+    let signature = this.sign(username);
+    console.log("signature: "+ signature)
   }
 
   logout() {
     // remove user from local storage to log user out
     sessionStorage.removeItem('user');
+  }
+
+  sign(plaintext: string): string {
+    let session = JSON.parse(sessionStorage.getItem('user'));
+    var sig = new KJUR.crypto.Signature({"alg": "SHA256withRSA"});
+    sig.init(session.key);
+    sig.updateString(plaintext);
+    var hSigVal = sig.sign();
+    return hSigVal;
   }
 }

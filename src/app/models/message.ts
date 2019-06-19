d6f04876b0318a67efa9347ebd2c89efedbe00ac
MrSpaceblaze <32390661+MrSpaceblaze@users.noreply.github.com>
2019-06-19 10:16:28 +0200

    refactored code to work with backend

--- src/app/models/message.ts
+++ src/app/models/message.ts
@@ -1,17 +1,14 @@
 export class Message implements JSON{
     [Symbol.toStringTag]: string;
     chat:string;
-    time: Date;
-    text: string;
+    message: string;
     username: string;
     parse(s:string){
         let j = JSON.parse(s);
-        this.chat = j.chat;
-        this.time = new Date(j.time);
-        this.text = j.text;
+        this.message = j.text;
         this.username = j.username;
     }
     public stringify(){
-        return '{ "chat": "' + this.chat + '", "text": "' + this.text + '", "username": "' + this.username +'" }'
+        return '{ "message": "' + this.message + '", "username": "' + this.username +'" }'
     }
 }
--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -7,17 +7,18 @@ import { Observable } from 'rxjs';
     providedIn: 'root'
 })
 /* MessageService expects to get chat messages over an event with the id of the stream in question. *
- * It sends messages using 'sendMessage' as an event, for the server to handle by reading the id of *
- * the messsage. It fires off a single getAll request at load to get all older messages in the chat */
+ * It sends messages using 'sendMessage' as an event, for the server to handle by reading the same event *
+ * It can fire off a single getAll request at load to get all older messages in the chat */
 export class MessageService {
     gottenMessages: Observable<Message[]>;
     private chatId: string;
     constructor(private socket: Socket) {
     }
-    public getObservable(chatId:string){
+    public getObservable(chatId:string, username: string){
         this.chatId = chatId;
+        this.socket.emit('join',"{ room:" + chatId + ", username:" + username + "}")
         return new Observable((observer) => {
-            this.socket.on(chatId, (data) => {
+            this.socket.on('sendMessage', (data) => {
                 let message = new Message();
                 message.parse(data);
                 observer.next(message);
